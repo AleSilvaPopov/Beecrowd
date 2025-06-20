46a2399c452e73635cad3bb68e9cab363b2641a7
Alexandre da Silva Popov <alexandre.popov406@academico.ufgd.edu.br>
2025-06-19 21:07:44 -0400

    Exercício 1763

---
+++ Exercicios/4-ESTRUTURAS E BIBLIOTECAS/bee1763.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+int main(){
+    unordered_map<string, string> felizNatal;
+
+    felizNatal = {
+        {"brasil", "Feliz Natal!"},
+        {"alemanha", "Frohliche Weihnachten!"},
+        {"austria", "Frohe Weihnacht!"},
+        {"coreia", "Chuk Sung Tan!"},
+        {"espanha", "Feliz Navidad!"},
+        {"grecia", "Kala Christougena!"},
+        {"estados-unidos", "Merry Christmas!"},
+        {"inglaterra", "Merry Christmas!"},
+        {"australia", "Merry Christmas!"},
+        {"portugal", "Feliz Natal!"},
+        {"suecia", "God Jul!"},
+        {"turquia", "Mutlu Noeller"},
+        {"argentina", "Feliz Navidad!"},
+        {"chile", "Feliz Navidad!"},
+        {"mexico", "Feliz Navidad!"},
+        {"antardida", "Merry Christmas!"},
+        {"canada", "Merry Christmas!"},
+        {"irlanda", "Nollaig Shona Dhuit!"},
+        {"belgica", "Zalig Kerstfeest!"},
+        {"italia", "Buon Natale!"},
+        {"libia", "Buon Natale!"},
+        {"siria", "Milad Mubarak!"},
+        {"marrocos", "Milad Mubarak!"},
+        {"japao", "Merii Kurisumasu!"}
+    };
+
+    string entrada;
+    while(cin >> entrada){
+        if (felizNatal.find(entrada) == felizNatal.end())
+            cout << "--- NOT FOUND ---\n";
+
+        else cout << felizNatal[entrada] << '\n';
+
+        return 0;
+    }
+}
