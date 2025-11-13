d0b51bfffcfbf6911e249a77373719dfbf981575
Alexandre da Silva Popov <alexandre.popov406@academico.ufgd.edu.br>
2025-11-12 22:32:04 -0400

    Exercício AD-HOC bee1104

--- Exercicios/2-AD-HOC/bee1104.cpp
+++ Exercicios/2-AD-HOC/bee1104.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
+#include <set>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
+    int a, b;
 
-    
+    while(cin >> a >> b && a != 0 && b != 0){
+        set<int> alice, beatriz;
+        int carta;
+
+        for(int i=0; i<a; i++){
+            cin >> carta;
+            alice.insert(carta);
+        }
+
+        for(int i=0; i<b; i++){
+            cin >> carta;
+            beatriz.insert(carta);
+        }
+        int contBeatriz=0;
+        for(int c : beatriz){
+            if(alice.find(c) == alice.end()) contBeatriz++;
+        }
+
+        int contAlice=0;
+        for(int c : alice){
+            if(beatriz.find(c) == beatriz.end()) contAlice++;
+        }
+
+        if(contAlice > contBeatriz) cout << contBeatriz << endl;
+
+        else cout << contAlice << endl;
+    }
 
     return 0;
 }
